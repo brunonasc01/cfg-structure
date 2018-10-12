35945db1c3781c33c4042931666f93e7d7f5edba
Bruno Nascimento <brunonasc@hotmail.com.br>
2018-10-12 08:33:36 -0300

    base commit

---
+++ src/br/com/cfg/model/DataTable.java
@@ -0,0 +1,12 @@
+package br.com.cfg.model;
+
+public class DataTable {
+
+	public String getAttributeValue(String mnemonico) {
+		return null;
+	}
+	
+	public void setAttributeValue(String mnemonico, String valor) {
+		
+	}
+}
